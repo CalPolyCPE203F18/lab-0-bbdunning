d92fc62c2c19c3b0deffbf241c612e4f704680f5
Brendan Dunning <dev@brendandunning.com>
2018-09-24 19:36:02 -0700

    Initial commit

--- Lab00.java
+++ Lab00.java
@@ -1,8 +1,10 @@
 public class Lab00
 {
-   public static void main(String[] args)
-   {
-
+    public static void main(String[] args)
+    {
+        int x = 5;
+        String y = "hello";
+        double z = 
    
-   }
+    }
 }
